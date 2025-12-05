ba414fd033aea62a101f45976b2731c5a35ff887
ushalbansode-ai <ushalbansode@gmail.com>
2025-12-05 15:23:41 +0530

    Delete src directory

--- src/analytics/__init__.py
+++
@@ -1,6 +0,0 @@
-from .oi_skew import calculate_oi_skew
-from .volume_oi import detect_volume_spike
-from .gamma_exposure import compute_gamma_exposure
-from .reversals import detect_reversal
-from .utils import zscore
-
--- src/analytics/utils.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-
-def zscore(series: pd.Series) -> pd.Series:
-    """Returns z-score normalized series."""
-    return (series - series.mean()) / (series.std() + 1e-9)
-  
